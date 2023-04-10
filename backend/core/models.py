364234134b572750b7307964ffdf7a4322facdf5
Anes Memisevic <anesmemisevic23@gmail.com>
2023-04-10 22:58:19 +0300

    add API's documentation in UI, deleting business - deletes their products as well

--- backend/core/models.py
+++ backend/core/models.py
@@ -22,7 +22,8 @@ class CustomerProfile(models.Model):
 class Product(models.Model):
     # might set to CASCASE
     # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    business = models.ForeignKey(BusinessProfile, on_delete=models.SET_NULL, null=True)
+    # business = models.ForeignKey(BusinessProfile, on_delete=models.SET_NULL, null=True)
+    business = models.ForeignKey(BusinessProfile, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
 
     image = models.ImageField(null=True, blank=True)
