ceb44aa33d9fd829640f5ecfffe8a3cb995ed0f7
sude-sahin <ssudesahin@gmail.com>
2023-04-16 13:08:52 +0300

    FOOD-31 <changes on packages & routing>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,18 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Login from "./pages/Login-Register/Login";
 import Register from "./pages/Login-Register/Register";
+import RegisterBusiness from "./pages/Login-Register/RegisterBusiness";
+
 import Market from "./pages/Market";
 import Restaurant from "./pages/Restaurant/Restaurant";
 
+import UserProfile from "./pages/Profile/UserProfile";
+
+import EditProfile from "./pages/Profile/EditProfile";
+
+import EditMenu from "./pages/BusinessMenu/EditMenu"
+
+import AddProduct from "./pages/BusinessMenu/AddProduct";
 
 
 function App() {
@@ -26,9 +35,16 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/register-business" element={<RegisterBusiness />} />
             <Route path="/market" element={<Market />} />
             <Route path="/restaurant" element={<Restaurant />} />
+
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/profile/edit" element={<EditProfile />} />
+
+            <Route path="/editmenu" element={<EditMenu />} />
+            <Route path="/editmenu/add-product" element={<AddProduct />} />
         </Routes>
     );
 
