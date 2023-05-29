da89f6c908c5d5f96cc890191a79c9e49027a487
Anes Memisevic <anesmemisevic23@gmail.com>
2023-05-30 02:23:55 +0300

    hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-i%3c6z4mup7siaxxc(p4&h$xjqjcq#cajf8+f)zlm20o^-r)n3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "foodrush-demo.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
