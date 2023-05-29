9fa2f89fd7fffadfb32e08ccd7d9b700bbd75190
Anes Memisevic <anesmemisevic23@gmail.com>
2023-05-30 02:27:59 +0300

    change routs

--- backend/urls.py
+++ backend/urls.py
@@ -58,9 +58,9 @@ urlpatterns = [
     # ),
     re_path("", TemplateView.as_view(template_name="index.html")),
 ]
-for route in react_routes:
-    urlpatterns += [
-        path("{}".format(route), TemplateView.as_view(template_name="index.html"))
-    ]
+# for route in react_routes:
+#     urlpatterns += [
+#         path("{}".format(route), TemplateView.as_view(template_name="index.html"))
+#     ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
