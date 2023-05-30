699dfa017ab1333af69ee48ab4befa4c40d3c854
Anes Memisevic <anesmemisevic23@gmail.com>
2023-05-30 03:00:43 +0300

    edits

--- backend/settings.py
+++ backend/settings.py
@@ -205,16 +205,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 
 # region custom-media
 
+STATIC_URL = "/static/"
 MEDIA_URL = "/images/"
 # MEDIA_ROOT = "static/images"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 # STATIC_ROOT = "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
